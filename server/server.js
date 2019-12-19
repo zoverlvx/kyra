c6a393c7e89cc38bff0293e2085e825fb0fc1a21
zoverlvx <zoverhulser@gmail.com>
2019-12-19 00:29:52 +0000

    Going to try other machine

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const server = require("express")();
+const request = require("request");
+const KEY = "AIzaSyDvSaImRfPecutxUo833sNROdjbhaDPYQQ";
+const CHANNEL_ID = "OCvO6uJUVJQ6SrATfsWR5_aA";
+const URL = `https://www.googleapis.com/youtube/v3/search?key=${KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=20`
+
+function makeAPICall(cb) {
+    request(URL, {json: true}, function(err, res, body) {
+        if (err) return cb(err);
+        return cb(body);
+    });
+}
 
 server.get("/", function(req, res) {
-    res.send("Server is running");
+    makeAPICall(function(response) {
+        res.write(JSON.stringify(response));
+        res.end();
+    });
 });
 
 module.exports = server;
