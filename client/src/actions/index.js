3468f783d1f343a45ef8aaad9884b5d396d75f08
zoverlvx <zoverhulser@gmail.com>
2020-01-03 12:11:35 -0600

    Extends option to specify number of max results from API.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,14 +7,52 @@ export const GET_CHANNEL_COMPLETE = "GET_CHANNEL_COMPLETE";
 
 const KEY = config.YT_KEY;
 const CHANNEL_ID="UCvO6uJUVJQ6SrATfsWR5_aA";
-const MAX_RESULTS=12;
-const URL = `https://www.googleapis.com/youtube/v3/search?type=video&key=${KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`;
+//const MAX_RESULTS=12;
+//const URL = `https://www.googleapis.com/youtube/v3/search?type=video&key=${KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`;
 
 
-export function getChannel(token) {
+
+
+
+export function getChannel(options) {
+	
+	let URL = `https://www.googleapis.com/youtube/v3/search?type=video&key=${KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date`;
+	
+	// if options are provided
+	if (options) {
+		// if there is a page token, append it to the end.
+		if (options.token) {
+			URL += `&pageToken=${options.token}`;
+		}
+		// if there isn't a maxResults option, default to 12.
+		if (!options.maxResults) {
+			URL += "&maxResults=12";
+		}
+		// if there is a maxResults option, append it to the end.
+		if (options.maxResults && typeof options.maxResults === "number") {
+			URL += `&maxResults=${options.maxResults}`;
+		}
+	}	
+
+	// if no options provided
+	if (!options) {
+		// default maxResults to 12 and append to the end.
+		URL += "&maxResults=12";
+	}
+	
+
+	
+
+	
+
+	
+	const channel = axios(URL);
+
+	/*
 	const channel = token 
 	? axios.get(URL + `&pageToken=${token}`)
 	: axios.get(URL);
+	*/
 
 	return function(dispatch) {
 		dispatch({type: GET_CHANNEL_INIT});
