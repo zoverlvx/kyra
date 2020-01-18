1f2514a995ed76cf0403a9baac0a21aac0184acd
zoverlvx <zoverhulser@gmail.com>
2020-01-18 18:05:35 -0500

    Fixes thumbnail sizes

--- client/src/components/Thumbnail/Thumbnail.js
+++ client/src/components/Thumbnail/Thumbnail.js
@@ -1,16 +1,9 @@
 import React from "react";
 import { makeStyles, GridListTile } from "@material-ui/core";
-/*
-import { Image } from "semantic-ui-react";
-
-export default function({video}) {
-	return <Image src={video.snippet.thumbnails.medium.url} />
-}
-*/
 
 const useStyles = makeStyles(theme => ({
 	gridList: {
-		width: 300,
+		width: 400,
 		height: 225,
 		paddingTop: "10px",
 		listStyleType: "none"
