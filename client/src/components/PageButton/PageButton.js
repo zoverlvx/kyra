ca6cc54dd335dddc4c98777de4577361dbce6d94
zoverlvx <zoverhulser@gmail.com>
2020-01-19 02:45:52 -0500

    Handles scrolling through current set of thumbnails

--- client/src/components/PageButton/PageButton.js
+++ client/src/components/PageButton/PageButton.js
@@ -1,8 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getChannel } from "../../actions";
+//import { getChannel } from "../../actions";
 import { Button } from "@material-ui/core";
 
+export default function (props) {
+	if (props.toPrevious) {
+		return (
+			<Button
+				color="primary"
+				variant="outlined"
+				onClick={() => props.toPrevious()}
+			>
+				Previous Page
+			</Button>
+		)
+	}
+
+	if (props.toNext) {
+		return (
+			<Button
+				color="primary"
+				variant="outlined"
+				onClick={() => props.toNext()}
+			>
+				Next Page
+			</Button>
+		)
+	}
+	
+	return null;
+}
+
+/*
 export default connect(null, {getChannel})(function(props) {
 	const { prevPage, nextPage, getChannel} = props;
 	if (prevPage) { 
@@ -10,7 +39,7 @@ export default connect(null, {getChannel})(function(props) {
 			<Button
 				color="primary"
 				variant="outlined"
-				onClick={() => getChannel({ token: prevPage })}	
+				onClick={() => 	getChannel({ token: prevPage })}	
 			>
 				Previous Page
 			</Button>
@@ -29,3 +58,4 @@ export default connect(null, {getChannel})(function(props) {
 	}
 	return null;
 });
+*/
