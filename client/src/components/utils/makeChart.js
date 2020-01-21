fa6af95587e0ca82ce5f9e03476ba54ba87807b6
zoverlvx <zoverhulser@gmail.com>
2020-01-21 00:39:35 -0500

    Shows chronology of videos published per day every month of the year.

--- client/src/components/utils/makeChart.js
+++ client/src/components/utils/makeChart.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Chart } from "react-google-charts";
+import { ButtonGroup } from "@material-ui/core";
+import makeButtons from "../utils/makeButtons.js";
 
 export default function(options) {
 
@@ -10,40 +12,66 @@ export default function(options) {
 		// sorts dates chronologically
 		const chronological = [...publishedDates].sort();
 
+		const getNext = () => props.getChannel({
+			token: props.nextPageToken
+		});
+
+		const getPrevious = () => props.getChannel({
+			token: props.prevPageToken
+		});
+
+		const conditions = {
+			forNext: props.nextPageToken,
+			forPrevious: props.prevPageToken
+		};
+
 
 		return (
-			<Chart
-				width={options.width}
-				height={options.height}
-				chartType={options.chartType}
-				loader={<div>Loading</div>}
-				data={[
-					[
-						{
-							type: "date", 
-							id: "Date"
-						}, 
-						{
-							type: "number", 
-							id:"Produced"
+			<>
+				<Chart
+					width={options.width}
+					height={options.height}
+					chartType={options.chartType}
+					loader={<div>Loading</div>}
+					data={[
+						[
+							{
+								type: "date", 
+								id: "Date"
+							}, 
+							{
+								type: "number", 
+								id:"Produced"
+							}
+						],
+						//map through and spread everything else below
+						...chronological.map(function(date, i) {
+							return [
+								new Date(date), 
+								1
+							]
+						})
+					]}
+					options={{
+						title: "YouTube Videos Produced",
+						colorAxis: {
+							maxValue: 1,
+							minValue: 0
 						}
-					],
-					//map through and spread everything else below
-					...chronological.map(function(date, i) {
-						return [
-							new Date(date), 
-							1
-						]
-					})
-				]}
-				options={{
-					title: "YouTube Videos Produced",
-					colorAxis: {
-						maxValue: 1,
-						minValue: 0
+					}}
+				/>
+				<ButtonGroup
+					color="primary"
+					aria-label="outlined primary button group"
+				>
+					{
+						makeButtons(
+							{getPrevious, getNext},
+							conditions
+						)
 					}
-				}}
-			/>
+				</ButtonGroup>
+			</>
 		);	
 
 	}
