54edb8015ebca255ed6d1f30333a0f4f655b62c6
zoverlvx <zoverhulser@gmail.com>
2020-01-26 20:38:34 -0500

    Refactors with react-redux hooks.

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -5,9 +5,48 @@ import { getChannel } from "../actions";
 import ThumbnailContainer from "./ThumbnailContainer/ThumbnailContainer";
 import Calendar from "./Calendar/Calendar.js";
 import Navigation from "./Navigation/Navigation.js";
+import { useDispatch } from "react-redux";
 
+
+// uses react-redux hooks
+export default function() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		// get channel contents on first mount
+		dispatch(getChannel());
+		const refresh = setInterval(() => {
+			// renew at 12:01am every day
+			const today = new Date();
+			const currentTime = `${today.getHours()}:${today.getMinutes()}`;
+			if (currentTime === "00:01") {
+				dispatch(getChannel());
+			}
+		}, 20000)
+		return () => clearInterval(refresh)
+	}, [getChannel]);
+
+	return (
+		<>
+			<Route 
+				exact path="/"
+			>
+				<Navigation to="/calendar" text="Go to Calendar" />
+				<ThumbnailContainer />
+				
+			</Route>
+			<Route 
+				path="/calendar" 
+			>
+				<Navigation to="/" text="Go to Thumbnails" />
+				<Calendar />
+			</Route>
+		</>
+	);
+}
 		
-
+/*
+// uses react-redux connect
 export default connect(
 	null, 
 	{getChannel}
@@ -18,7 +57,7 @@ export default connect(
 		// Get channel contents on first mount
 		getChannel(); 
 		const refresh = setInterval(() => {
-			/* renew at 12:01am every day */
+			// renew at 12:01am every day
 			const today = new Date();
 			const currentTime = `${today.getHours()}:${today.getMinutes()}`;
 			if (currentTime === "00:01") {
@@ -46,3 +85,4 @@ export default connect(
 		</>
 	);
 });
+*/
