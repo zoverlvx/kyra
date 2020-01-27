b5091144d29179c89b2da42d8183bceb0962f8aa
zoverlvx <zoverhulser@gmail.com>
2020-01-26 21:44:31 -0500

    Removes unneeded dependency

--- client/src/components/ThumbnailContainer/ThumbnailContainer.js
+++ client/src/components/ThumbnailContainer/ThumbnailContainer.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import { connect } from "react-redux";
 import { ButtonGroup } from "@material-ui/core";
 import {getChannel} from "../../actions";
 import { useDispatch, useSelector } from "react-redux";
