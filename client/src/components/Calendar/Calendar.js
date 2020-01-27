6300ec97b0ee15b615eadd149009f515b647ae82
zoverlvx <zoverhulser@gmail.com>
2020-01-26 21:43:14 -0500

    Refactors with react-redux hooks and removes unnecessary hoc

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,6 +1,11 @@
-import { connect } from "react-redux";
+//import { connect } from "react-redux";
+//import makeChart from "../utils/makeChart.js";
+import React from "react";
+import { Chart } from "react-google-charts";
 import { getChannel } from "../../actions";
-import makeChart from "../utils/makeChart.js";
+import { useSelector, useDispatch } from "react-redux";
+import { ButtonGroup } from "@material-ui/core";
+import makeButtons from "../utils/makeButtons.js";
 
 /*
 
@@ -12,6 +17,89 @@ If the data values are all positive, the colors will range from white to blue, w
 
 */
 
+
+export default function() {
+	// maps state to props
+	const videos = useSelector(state => state.channel.items);
+	const nextPageToken = useSelector(state => state.channel.nextPageToken);
+	const prevPageToken = useSelector(state => state.channel.prevPageToken);
+	const dispatch = useDispatch();
+
+	const getPublishedDates = item => item.snippet.publishedAt;
+	// gets published dates of videos
+	const publishedDates = [...videos].map(getPublishedDates);
+
+	// sorts dates chronologically
+	// TODO: this might be negatively affecting load time
+	// might not even need this in between how the db content is already set up and how the calendar component displays the data
+	const chronological = [...publishedDates].sort();
+
+	const getNext = () => dispatch(getChannel({
+		token: nextPageToken
+	}));
+	
+	const getPrevious = () => dispatch(getChannel({
+		token: prevPageToken
+	}));
+
+	const conditions = {
+		forNext: nextPageToken,
+		forPrevious: prevPageToken
+	};
+
+	return (
+		<>
+			<Chart 
+				width={1000}
+				height={350}
+				chartType={"Calendar"}
+				loader={<div>Loading</div>}
+				data={[
+					[
+						{
+							type: "date",
+							id: "Date"
+						},
+						{
+							type: "number",
+							id: "Produced"
+						}
+					],
+
+					//map through and spread everything else below
+					...chronological.map(function(
+						date
+					) {
+						return [
+							new Date(date),
+							1 // sets it to blue
+						];
+					})
+				]}
+				options={{
+					title: "YouTube Videos Produced",
+					colorAxis: {
+						maxValue: 1,
+						minValue: 0
+					}
+				}}
+			/>
+			<ButtonGroup
+				color="primary"
+				aria-label="outlined primary button group"
+			>
+				{
+					makeButtons(
+						{getPrevious, getNext},
+						conditions
+					)
+				}
+			</ButtonGroup>
+		</>
+	);
+}
+
+/*
 const Calendar = makeChart({
 	width: 1000, 
 	height: 350, 
@@ -29,3 +117,4 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, {getChannel})(Calendar);
+*/
