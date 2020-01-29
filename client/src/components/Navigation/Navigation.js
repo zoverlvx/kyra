88a259b45ba70401618ede143dc255372217206c
zoverlvx <zoverhulser@gmail.com>
2020-01-29 00:21:18 -0500

    Moves buttons to navigation area and refactors.

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,26 +1,47 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { 
+	AppBar, 
+	Toolbar, 
+	Typography, 
+	ButtonGroup,
+	Box
+} from "@material-ui/core";
+import { spacing } from "@material-ui/system";
 import { NavLink } from "react-router-dom";
+import makeButtons from "../utils/makeButtons.js";
 
-export default function({to, text}) {
+export default function({to, text, conditions, actionHandler}) {
+
 	const navLinkStyle = {
 		color: "white",
 		textDecoration: "none"
 	};
 	return (
-		<AppBar color="primary" position="static">
-			<Toolbar>
-				<Typography
-					color="inherit"
-				>
-					<NavLink 
-						style={navLinkStyle}
-						to={to}
+		<>
+			<AppBar color="primary" position="static">
+				<Toolbar>
+					<Typography
+						color="inherit"
 					>
-						{text}
-					</NavLink>
-				</Typography>
-			</Toolbar>
-		</AppBar>
+						<NavLink 
+							style={navLinkStyle}
+							to={to}
+						>
+							{text}
+						</NavLink>
+					</Typography>
+					<Box ml={2}>
+						<ButtonGroup
+							color="inherit"
+							aria-label="outlined primary button group"
+						>
+							{
+								makeButtons(actionHandler, conditions)
+							}
+						</ButtonGroup>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</>
 	);
 }
